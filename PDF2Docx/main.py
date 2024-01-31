e586dca505f99a6bd297009315c9b345bae29539
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-31 10:19:26 +0545

    Initial

--- PDF2Docx/main.py
+++ PDF2Docx/main.py
@@ -1,1 +1,20 @@
-print("Testing Hello WOrld")
+from pdf2docx import Converter
+import os
+
+pdf_name=input("Enter the name of the pdf file: ")
+if not pdf_name.endswith(".pdf"):
+    pdf_name=pdf_name+".pdf"
+if not os.path.exists(pdf_name):
+    print("File does not exist")
+    exit()
+docx_name=input("Enter the name of the docx file: ")
+if not docx_name.endswith(".docx"):
+    docx_name=docx_name+".docx"
+
+try:
+    cv = Converter(pdf_name)
+    cv.convert(docx_name)   
+    cv.close()
+    print("Converted Successfully")
+except:
+    print("Failed To Convert")
