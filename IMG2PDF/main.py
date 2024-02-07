269a79b5cf78d42012c988e93d9b6684a920cffe
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-07 07:57:39 +0545

    Initial

--- IMG2PDF/main.py
+++ IMG2PDF/main.py
@@ -2,20 +2,31 @@ import img2pdf
 import os
 import pathlib
 
-# opening from filename
-images=['test.jpg','test1.jpg','test2.jpg']
-with open("final.pdf","wb") as f:
-	f.write(img2pdf.convert(images))
 
-# convert all files ending in .jpg inside a directory
-# dirname = "/path/to/images"
-# imgs = []
-# for fname in os.listdir(dirname):
-# 	if not fname.endswith(".jpg"):
-# 		continue
-# 	path = os.path.join(dirname, fname)
-# 	if os.path.isdir(path):
-# 		continue
-# 	imgs.append(path)
-# with open("name.pdf","wb") as f:
-# 	f.write(img2pdf.convert(imgs))
+
+file_path=input("Enter the Name of the Images(Provide Space between if more than One Images): ")
+ImagesCollections = list(filter(None,(file_path.split(" "))))
+print(ImagesCollections)
+
+# check that every data file is ends with .jpg and add .jpg if not ends with .jpg
+for i in ImagesCollections:
+	if not i.endswith(".jpg"):
+		ImagesCollections[ImagesCollections.index(i)] = i + ".jpg"
+
+# check that the file exist or not 
+for i in ImagesCollections:
+	if not os.path.exists(i):
+		print(f"File {i} not found")
+		exit(1)
+
+output_file = input("Enter the Name of the Output File: ")
+if not output_file.endswith(".pdf"):
+	output_file = output_file + ".pdf"
+
+try:
+	with open(output_file,"wb") as f:
+		print("Converting to PDF...")
+		f.write(img2pdf.convert(ImagesCollections))
+		print("PDF Created Successfully")
+except Exception as e:
+	print("Failed to Convert to PDF")
