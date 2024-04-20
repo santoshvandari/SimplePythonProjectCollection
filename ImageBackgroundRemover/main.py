7696c435dfdb0422c0a6d8d60dcb8c88fa5bf8a5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-04-20 11:08:42 +0545

    Main.py File Created

--- ImageBackgroundRemover/main.py
+++ ImageBackgroundRemover/main.py
@@ -2,7 +2,6 @@ from rembg import remove
 from PIL import Image
 import os
 
-# input_path = 'input.jpeg'
 input_path = (input("Enter a Image Name(with Extension): ")).lower().strip()
 if not (input_path and input_path.endswith((".jpeg",".jpg",".png"))):
     print("Invalid File Name.")
@@ -10,7 +9,6 @@ if not (input_path and input_path.endswith((".jpeg",".jpg",".png"))):
 if not os.path.exists(input_path):
     print("File Doesn't Exists.")
     exit(1)
-# output_path = 'output.png'
 output_path=input("Enter a Output File Name: ")
 if not output_path.endswith(".png"):
     if not output_path:
